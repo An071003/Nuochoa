7b9e997dd2282ecc09322c13df5bc3a7062d78a9
Pham-Dang-Khoi <138744608+Pham-Dang-Khoi@users.noreply.github.com>
2024-12-29 14:47:22 +0700

    Update component video

--- src/app/pages/Home/partials/Video.js
+++ src/app/pages/Home/partials/Video.js
@@ -2,9 +2,8 @@ import React from 'react'
 
 export default function Video() {
   return (
-    <div>
-      <video muted autoPlay playsInline loop>
-        <source src="https://cdn.shopify.com/videos/c/o/v/82ac5b452ae34407aacd35b0bfe2455d.mp4" type="video/mp4" />
+    <div className='mt-4'>
+      <video muted autoPlay playsInline loop preload="auto">
         <source src="https://cdn.shopify.com/videos/c/o/v/82ac5b452ae34407aacd35b0bfe2455d.mp4" type="video/mp4" />
         Your browser does not support the video tag.
       </video>
