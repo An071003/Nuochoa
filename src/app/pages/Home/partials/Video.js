1bfcf66960eaf2cf250ca68867f460136cab9dc3
An071003 <pekkidjj123@gmail.com>
2024-12-29 11:28:34 +0700

    video

--- src/app/pages/Home/partials/Video.js
+++ src/app/pages/Home/partials/Video.js
@@ -2,6 +2,12 @@ import React from 'react'
 
 export default function Video() {
   return (
-    <div>Video</div>
+    <div>
+      <video muted autoPlay playsInline loop>
+        <source src="https://cdn.shopify.com/videos/c/o/v/82ac5b452ae34407aacd35b0bfe2455d.mp4" type="video/mp4" />
+        <source src="https://cdn.shopify.com/videos/c/o/v/82ac5b452ae34407aacd35b0bfe2455d.mp4" type="video/mp4" />
+        Your browser does not support the video tag.
+      </video>
+    </div>
   )
 }
