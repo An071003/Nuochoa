9905ddae2c36424c65d2cf1e25a11efb582447ba
An071003 <147960523+An071003@users.noreply.github.com>
2025-01-04 19:33:42 +0700

    Update MainRoutes.js

--- src/app/routes/MainRoutes.js
+++ src/app/routes/MainRoutes.js
@@ -17,7 +17,6 @@ import UserLayout from "../layouts/UserLayout/UserLayout";
 import ForgotPassword from "../components/ForgotPassword/ForgotPassword";
 import ResetPassword from "../components/ResetPassword";
 import PrivateRoute from "./PrivateRoute"; // Đảm bảo import đúng PrivateRoute
-import Products from "src/Products.js";  // Import Products.js
 
 const MainRoutes = () => {
   return (
@@ -42,9 +41,6 @@ const MainRoutes = () => {
           <Route path="/checkout/payment" element={<PrivateRoute><CheckoutPayment /></PrivateRoute>} />
           <Route path="/user" element={<PrivateRoute><UserPage /></PrivateRoute>} />
         </Route>
-
-        {/* Route cho danh sách sản phẩm (không yêu cầu bảo vệ nếu cần) */}
-        <Route path="/products" element={<Products />} />
 
         {/* Route fallback cho trang không tìm thấy */}
         <Route path="*" element={<PageNotFound />} />
