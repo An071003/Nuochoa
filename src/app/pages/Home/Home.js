38b266f5215ff68a7c5d11cf49af539c08ff57d6
An071003 <147960523+An071003@users.noreply.github.com>
2025-01-11 16:41:25 +0700

    Update Home.js

--- src/app/pages/Home/Home.js
+++ src/app/pages/Home/Home.js
@@ -6,16 +6,6 @@ import Brand from "./partials/Brand";
 import Video from "./partials/Video";
 
 export default function Home() {
-
-
-  return (import React from "react";
-import { sCount } from "./homeStore";
-import Carousel from "./partials/Carousel";
-import ProductList from "./partials/ProductList";
-import Brand from "./partials/Brand";
-import Video from "./partials/Video";
-
-export default function Home() {
   return (
     <div className="bg-white">
       <div className="w-full min-h-[400px] pb-24">
@@ -32,17 +22,6 @@ export default function Home() {
 
       <div className="py-12 sm:py-16 md:py-20">
         <ProductList />
-      </div>
-    </div>
-  );
-}
-
-    <div>
-      <div className="w-full h-full min-h-96 pb-24 bg-white py-0">
-        <Carousel />
-        <Brand />
-        <Video/>
-        <ProductList/>
       </div>
     </div>
   );
