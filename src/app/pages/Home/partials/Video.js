f16a0a2c42115b46f60d8ac4c3e81305967cae56
An071003 <147960523+An071003@users.noreply.github.com>
2025-01-11 16:34:12 +0700

    Update Video.js

--- src/app/pages/Home/partials/Video.js
+++ src/app/pages/Home/partials/Video.js
@@ -2,11 +2,23 @@ import React from 'react'
 
 export default function Video() {
   return (
-    <div className='mt-4'>
-      <video muted autoPlay playsInline loop preload="auto">
-        <source src="https://cdn.shopify.com/videos/c/o/v/82ac5b452ae34407aacd35b0bfe2455d.mp4" type="video/mp4" />
-        Your browser does not support the video tag.
-      </video>
+    <div className="mt-4 w-full">
+      <div className="relative w-full h-0" style={{ paddingBottom: '56.25%' }}>
+        <video
+          className="absolute top-0 left-0 w-full h-full object-cover"
+          muted
+          autoPlay
+          playsInline
+          loop
+          preload="auto"
+        >
+          <source
+            src="https://cdn.shopify.com/videos/c/o/v/82ac5b452ae34407aacd35b0bfe2455d.mp4"
+            type="video/mp4"
+          />
+          Your browser does not support the video tag.
+        </video>
+      </div>
     </div>
   )
 }
